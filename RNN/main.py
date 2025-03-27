0a791f033ad3094486849f140be58fc5ba0f6c0f
Benny Lay <benny.lay2003@gmail.com>
2025-03-27 09:55:25 +0100

    Remove RNN test files and add RNN notebook

--- RNN/main.py
+++
@@ -1,7 +0,0 @@
-from train import train_model
-from generate import generate_sentence
-import tensorflow as tf
-
-if __name__ == "__main__":
-    tokenizer, max_length, model = train_model()
-    print(generate_sentence("Bonjour", model, tokenizer, max_length))
