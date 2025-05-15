5a7a693621bb6bb44b2a4b1073dcc2c929b4a55b
Benny Lay <benny.lay2003@gmail.com>
2025-05-15 12:22:47 +0200

    Merged CNN and LSTM in model.py

---
+++ model.py
@@ -0,0 +1,83 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+embedding_size = 256
+units = 512
+vocab_size = 10000 #10 000 mots de vocabulaire différents
+max_caption_len = 50
+
+def encoder_model(embedding_size):
+    # Create the ResNet50 as backbone
+    backbone = tf.keras.applications.ResNet50(
+        weights='imagenet',
+        include_top=False,
+        input_shape=(224, 224, 3)
+    )
+    #freeze the weight of the backbone
+    for layer in backbone.layers:
+        layer.trainable = False
+    
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(224, 224, 3)),
+        backbone,
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(embedding_size, activation='relu')
+    ])
+    
+    return model
+
+
+def preprocess_image(img_path):
+    image = tf.io.read_file(img_path)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [224, 224])
+    image = image / 255.0
+    image = tf.expand_dims(image, 0)
+    return image
+
+
+def decoder_model():
+    # Entrée pour les caractéristiques de l'image
+    inputs1 = tf.keras.layers.Input(shape=(embedding_size,))
+    fc_input = tf.keras.layers.Dropout(0.5)(inputs1)
+    fc_input = tf.keras.layers.Dense(units, activation='relu')(fc_input)
+    
+    # Entrée pour la séquence de mots
+    inputs2 = tf.keras.layers.Input(shape=(max_caption_len,))
+    LSTM_input = tf.keras.layers.Embedding(vocab_size, embedding_size, mask_zero=True)(inputs2)
+    LSTM_input = tf.keras.layers.LSTM(units)(LSTM_input)
+    
+    # Fusion des deux branches
+    decoder = tf.keras.layers.add([fc_input, LSTM_input])
+    decoder = tf.keras.layers.Dense(256, activation='relu')(decoder)
+    outputs = tf.keras.layers.Dense(vocab_size, activation='softmax')(decoder)
+    
+    # Construction du modèle complet
+    model = tf.keras.Model(inputs=[inputs1, inputs2], outputs=outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    
+    return model
+
+img_path = r"C:\Users\benny\benny_perso\eseo\i2\S8\projet_synthèse\image-captioning\dataset\ms_coco_2017\test2017\000000000001.jpg"
+encoder = encoder_model(embedding_size)
+
+# Prétraitement de l'image et extraction des caractéristiques
+image = preprocess_image(img_path)
+vector = encoder(image)
+
+decoder = decoder_model()  
+
+
+caption = decoder.predict([vector, ])
+
+encoder.summary()
+decoder.summary()
+
+#print("vecteur caractéristique: ", vector)
+
+
+
+
+
