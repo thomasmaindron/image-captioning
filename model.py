197ad287ec068f44b0e6fed73714d55c6586413e
Benny Lay <benny.lay2003@gmail.com>
2025-05-19 00:13:46 +0200

    Added caption generation function and related functions in model.py

--- model.py
+++ model.py
@@ -1,11 +1,26 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
+from tensorflow.keras.preprocessing.text import Tokenizer, tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
 embedding_size = 256
 units = 512
 vocab_size = 10000 #10 000 mots de vocabulaire différents
 max_caption_len = 50
+
+# CHARGEMENT DU TOKENIZER
+with open(r".\dataset\ms_coco_2017\tokenizer.json") as f:
+    tokenizer_data = json.load(f)
+    tokenizer = tokenizer_from_json(tokenizer_data)
+
+def preprocess_image(img_path):
+    image = tf.io.read_file(img_path)
+    image = tf.io.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image, [224, 224])
+    image = image / 255.0
+    image = tf.expand_dims(image, 0)
+    return image
 
 def encoder_model(embedding_size):
     # Create the ResNet50 as backbone
@@ -17,28 +32,15 @@ def encoder_model(embedding_size):
     #freeze the weight of the backbone
     for layer in backbone.layers:
         layer.trainable = False
-    
-
     model = tf.keras.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(224, 224, 3)),
+        tf.keras.layers.InputLayer(shape=(224, 224, 3)),
         backbone,
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(embedding_size, activation='relu')
     ])
-    
     return model
 
-
-def preprocess_image(img_path):
-    image = tf.io.read_file(img_path)
-    image = tf.io.decode_jpeg(image, channels=3)
-    image = tf.image.resize(image, [224, 224])
-    image = image / 255.0
-    image = tf.expand_dims(image, 0)
-    return image
-
-
-def decoder_model():
+def decoder_model(vocab_size, embedding_size, max_caption_len):
     # Entrée pour les caractéristiques de l'image
     inputs1 = tf.keras.layers.Input(shape=(embedding_size,))
     fc_input = tf.keras.layers.Dropout(0.5)(inputs1)
@@ -60,22 +62,36 @@ def decoder_model():
     
     return model
 
-img_path = r"C:\Users\benny\benny_perso\eseo\i2\S8\projet_synthèse\image-captioning\dataset\ms_coco_2017\test2017\000000000001.jpg"
+img_path = r".\dataset\ms_coco_2017\test2017\000000000001.jpg"
 encoder = encoder_model(embedding_size)
-
-# Prétraitement de l'image et extraction des caractéristiques
-image = preprocess_image(img_path)
-vector = encoder(image)
-
-decoder = decoder_model()  
-
-
-caption = decoder.predict([vector, ])
-
-encoder.summary()
-decoder.summary()
-
-#print("vecteur caractéristique: ", vector)
+decoder = decoder_model(vocab_size, embedding_size, max_caption_len) 
+# # Prétraitement de l'image et extraction des caractéristiques
+# image = preprocess_image(img_path)
+# vector = encoder(image)
+# decoder = decoder_model()  
+# encoder.summary()
+# decoder.summary()
+def ind_to_word(integer, tokenizer):
+    for word, index in tokenizer: # ou tokenizer.word_index.items() à voir
+        if index == integer:
+            return word
+    return None
+def caption_generation(image_path, tokenizer, encoder, decoder, max_length=max_caption_len):
+        image = preprocess_image(image_path)
+        features = encoder(image)
+        text = "start"
+        for i in range(max_length):
+            sequence=tokenizer.texts_to_sequences([text])[0]
+            sequence=pad_sequences([sequence], maxlen=max_length)
+            y_pred=decoder.predict([features, sequence])
+            ind_pred = np.argmax(y_pred)
+            word = ind_to_word(ind_pred, tokenizer)
+            if word is None:
+                break
+            text += " " + word
+            if word == "end":
+                break
+        return text
 
 
 
